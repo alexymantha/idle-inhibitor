f173d0c349cc25c4a4063e8c071bd4b05861ac99
Alexy Mantha <alexy@mantha.dev>
2025-03-22 14:18:12 -0400

    setup nix
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- flake.nix
+++ flake.nix
@@ -7,23 +7,119 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+    let
+      nixosModule = { config, lib, pkgs, ... }:
+        let
+          cfg = config.services.idle-inhibitor;
+        in {
+          options.services.idle-inhibitor = {
+            enable = lib.mkEnableOption "Enables the idle inhibitor service";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${pkgs.system}.idle-inhibitor;
+              description = "Package to use for the service";
+            };
+
+            user = lib.mkOption {
+              type = lib.types.str;
+              default = "idle-inhibitor";
+              description = "User account under which the service runs";
+            };
+
+            group = lib.mkOption {
+              type = lib.types.str;
+              default = "idle-inhibitor";
+              description = "Group under which the service runs";
+            };
+
+            # Add any other configuration options your service needs
+            port = lib.mkOption {
+              type = lib.types.port;
+              default = 8080;
+              description = "Port to listen on";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            systemd.services.idle-inhibitor = {
+              description = "Idle Inhibitor Service";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+
+              serviceConfig = {
+                Type = "simple";
+                User = cfg.user;
+                Group = cfg.group;
+                ExecStart = "${cfg.package}/bin/idle-inhibitor";
+                Restart = "on-failure";
+                RestartSec = "5s";
+
+                # Hardening options
+                PrivateTmp = true;
+                ProtectSystem = "strict";
+                ProtectHome = true;
+                NoNewPrivileges = true;
+              };
+            };
+
+            # Create user and group for the service
+            users.users.${cfg.user} = lib.mkIf (cfg.user == "idle-inhibitor") {
+              isSystemUser = true;
+              group = cfg.group;
+              description = "Idle Inhibitor Service User";
+            };
+
+            users.groups.${cfg.group} = lib.mkIf (cfg.group == "idle-inhibitor") {};
+          };
         };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            wayland
-          ];
-          
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.wayland
-          ];
-        };
-      }
-    );
+    in 
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+          {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              wayland
+              zig
+            ];
+
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.wayland
+            ];
+          };
+
+          packages.idle-inhibitor = pkgs.stdenv.mkDerivation {
+            pname = "idle-inhibitor";
+            version = "0.0.1";
+
+            src = ./.; # Assumes source code is in the same directory as flake.nix
+
+            nativeBuildInputs = with pkgs; [
+              zig
+            ];
+
+            buildInputs = with pkgs; [
+              wayland
+            ];
+
+            buildPhase = ''
+            zig build -Doptimize=ReleaseSafe
+            '';
+
+            installPhase = ''
+            mkdir -p $out/bin
+            cp zig-out/bin/idle-inhibitor $out/bin/
+            '';
+          };
+
+          packages.default = self.packages.${system}.idle-inhibitor;
+        }
+      ) // {
+      nixosModules.default = nixosModule;
+    };
 }
 
